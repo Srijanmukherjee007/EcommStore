7197d6e4b8086b28e04b6fec184fea5d10347501
Srijanmukherjee007 <mukherjeesrijan2@gmail.com>
2021-06-10 19:09:24 +0530

    added static path

--- src/ecommercestore/settings.py
+++ src/ecommercestore/settings.py
@@ -105,13 +105,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = "/static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "public")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
