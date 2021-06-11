717dab8868b398e9319af82f936ee85d9056adcb
Srijanmukherjee007 <mukherjeesrijan2@gmail.com>
2021-06-11 13:51:42 +0530

    splitted into templates

--- src/core/views.py
+++ src/core/views.py
@@ -6,4 +6,12 @@ from .models import Item
 
 def item_list(request: HttpRequest):
     context = {"items": Item.objects.all()}
-    return render(request, "home-page.html", context)
+    return render(request, "home.html", context)
+
+
+def products(request: HttpRequest):
+    return render(request, "product.html")
+
+
+def checkout(request: HttpRequest):
+    return render(request, "checkout.html")
